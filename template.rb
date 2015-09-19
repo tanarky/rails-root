539e8f420e040d122b9a67e39028fe3ea9022d76
Satoshi Tanaka <tanarky@gmail.com>
2015-09-20 01:45:12 +0900

    fixed

--- template.rb
+++ template.rb
@@ -6,14 +6,31 @@ gem 'simple_form'
 gem 'ransack'
 gem 'kaminari'
 gem 'active_decorator'
-gem 'unicorn'
 
-remove_dir 'test'
+gem_group :development, :test do
+  gem 'capistrano'
+  gem 'capistrano-rbenv'
+  gem 'capistrano-rails'
+  gem 'capistrano-bundler'
+  gem 'quiet_assets'
+end
 
 # http://morizyun.github.io/blog/rails4-application-templates-heroku/
 Bundler.with_clean_env do
   run 'bundle install --path=.bundle --jobs=4'
+  run 'bundle exec cap install'
+  remove_dir 'test'
+  generate 'simple_form:install --bootstrap'
+  generate 'kaminari:config'
+  generate 'rspec:install'
+  run "echo '--color -f d' > .rspec"
 end
+
+# rails guide (ja): http://railsguides.jp/rails_application_templates.html
+# rails guide (en): http://guides.rubyonrails.org/rails_application_templates.html
+# rails composer: https://github.com/RailsApps/rails-composer
+# bundle exec rails new projects/tonight --skip-bundle -m /path/to/template.rb
+# bundle exec rake rails:template LOCATION=/path/to/template.rb
 
 #use_devise = if yes?('Use devise?')
 #               gem 'devise'
